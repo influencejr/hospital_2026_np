1fdefa2587647e7c04128370b62a03f2e94ce874
David Vovk <davidvovk@proton.me>
2026-02-09 00:19:35 +0400

    base program

---
+++ src/main/java/org/example/hospital_2026_np/Main.java
@@ -0,0 +1,13 @@
+package org.example.hospital_2026_np;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/hospital_2026_np/Hospital2026NpApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.hospital_2026_np;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hospital2026NpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
