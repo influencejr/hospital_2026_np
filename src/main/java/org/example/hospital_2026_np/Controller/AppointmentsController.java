4cf18951c4e8887d89ae95f4c339fe6350df6c99
David Vovk <davidvovk@proton.me>
2026-04-29 21:34:00 +0400

    fixed bugs2

--- src/main/java/org/example/hospital_2026_np/Controller/AppointmentsController.java
+++ src/main/java/org/example/hospital_2026_np/Controller/AppointmentsController.java
@@ -35,7 +35,7 @@ public class AppointmentsController {
     private final DoctorAvailabilityService doctorAvailabilityService;
     private final UserService userService;
 
-    final String attributeName = "appointments";
+    final static String attributeName = "appointments";
 
     @GetMapping("/appointments/{id}")
     public String getAppointmentsWithId(@PathVariable Long id,
